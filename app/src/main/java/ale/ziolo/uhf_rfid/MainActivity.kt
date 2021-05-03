1d1fed8ea88a551305c8201642b3bc39e0bfa4c3
aziolo <ale.m.ziolo@gmail.com>
2021-05-03 21:27:15 +0200

    Initial commit

---
+++ app/src/main/java/ale/ziolo/uhf_rfid/MainActivity.kt
@@ -0,0 +1,11 @@
+package ale.ziolo.uhf_rfid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
